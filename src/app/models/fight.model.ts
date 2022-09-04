97acce988de4a5bf2acc75bd8dbf7ae8287658de
Rafał Nazarko <rafal.nazarko@gmail.com>
2022-09-04 09:57:57 +0200

    Feature fights system (#10)
    * started creating planning fights view
    
    * add display tree fight
    
    * finish fights system

---
+++ src/app/models/fight.model.ts
@@ -0,0 +1,16 @@
+export interface Fight {
+  czas_zakonczenia: number | null
+  grupa_id: number
+  nastepna_walka_id: number | null
+  robot1_id: number | null
+  robot1_nazwa: string | null
+  robot1_uuid: string | null
+  robot2_id: number | null
+  robot2_nazwa: string | null
+  robot2_uuid: string | null
+  stanowisko_id: number | null
+  nazwa_stanowiska: string | null
+  walka_id: number
+  wygrane_rundy_robot1: number | null
+  wygrane_rundy_robot2: number | null
+}
