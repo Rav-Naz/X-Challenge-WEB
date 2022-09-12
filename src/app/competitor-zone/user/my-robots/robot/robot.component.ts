25d4e4db5565a5072fb5000d8fe16b83f4f47a96
Rafał Nazarko <rafal.nazarko@gmail.com>
2022-09-12 19:58:42 +0200

    add user fights and times view

--- src/app/competitor-zone/user/my-robots/robot/robot.component.ts
+++ src/app/competitor-zone/user/my-robots/robot/robot.component.ts
@@ -509,7 +509,9 @@ export class RobotComponent {
   }
 
   get getCategoryFigths() {
-    return this.robotFights?.filter(el => el.kategoria_id === this.selectedCategory).sort((a, b) => b.walka_id - a.walka_id).sort((a, b) => a.czas_zakonczenia - b.czas_zakonczenia);
+    const fights =  this.robotFights?.filter(el => el.kategoria_id === this.selectedCategory).sort((a, b) => new Date(a.czas_zakonczenia).getTime() - new Date(b.czas_zakonczenia).getTime());
+    console.log(fights)
+    return fights
   }
 
   get getGroupFigths() {
