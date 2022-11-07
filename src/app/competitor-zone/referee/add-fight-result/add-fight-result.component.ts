6146fe72ea1c2273a5bc84ead318b356a0231376
Rafał Nazarko <rafal.nazarko@gmail.com>
2022-11-07 12:28:41 +0100

    acc logotypes and future of active timetable and infos for streamers (#27)
    * change registration
    
    * add new settings options and correct mobile view
    
    * add sending documentation and movie link feature
    
    * prepare to deploy
    
    * add test rfid endpoints
    
    * Test
    
    * Test2
    
    * test3
    
    * Changed gh actions test name
    
    * registration finish (#4)
    
    * Fix user type selection (#5)
    
    * registration finish
    
    * fix user type selection
    
    * Feature counting visitors (#6)
    
    * registration finish
    
    * add counting visitors, test bramki request and few logotypes
    
    * Add sending messages via callapi (#7)
    
    * registration finish
    
    * add counting visitors, test bramki request and few logotypes
    
    * add sending messages via callapi
    
    * fix small bug in competitors view
    
    * add logos and documentation for freestyle
    
    * Feature fights system (#10)
    
    * started creating planning fights view
    
    * add display tree fight
    
    * finish fights system
    
    * add th movie
    
    * add results autosliding and fight view (#12)
    
    * positions management (#13)
    
    * add position management system
    
    * add position management from referee user view
    
    * Update environment.prod.ts
    
    * Feature referee fights (#14)
    
    * add times referee management
    
    * add referee fights view
    
    * add feature of active groups
    
    * add announcements
    
    * Update environment.prod.ts
    
    * Feature user fights and times (#15)
    
    * add user fights and times view
    
    * add few minor changes
    
    * add logotypes
    
    * fix user filter by categories and robots
    
    * add rfid card management panel (#16)
    
    * Update referee.service.ts
    
    * fix bug
    
    * add autocompletion checkbox for rfid card management panel
    
    * add huawei and forbot logotypes
    
    * add warning to competitors registered after first deadline
    
    * Feature timeline (#20)
    
    * nearly finish this shit
    
    * finish editable timetable and add logos
    
    * Update environment.prod.ts
    
    * fix timetable bug
    
    * add MEiN logo
    
    * change honorary logos order
    
    * change mein logo
    
    * new logo mein
    
    * add comments to time results and fix few bugs
    
    * add selectable rfid and gates options and add icons to rfid panel
    
    * add info
    
    * add tme logo
    
    * add sędziszów logo
    
    * add riddle info
    
    * add mrit logo
    
    * Update pl.json
    
    * Update en.json
    
    * add final logotypes
    
    * add active timetable and infos for streamers (#26)
    
    Co-authored-by: dornach-piotr-storek <piotr.storek@dornach.eu>

--- src/app/competitor-zone/referee/add-fight-result/add-fight-result.component.ts
+++ src/app/competitor-zone/referee/add-fight-result/add-fight-result.component.ts
@@ -7,6 +7,7 @@ import { RefereeService } from 'src/app/services/referee.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { UiService } from 'src/app/services/ui.service';
 import { Title } from '@angular/platform-browser';
+import { ActiveFightsService } from '../../../services/active_fights';
 
 @Component({
   selector: 'app-add-fight-result',
@@ -20,18 +21,20 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
 
   formUUID1: FormGroup;
   formUUID2: FormGroup;
+  formArena: FormGroup;
   formResult1: FormGroup;
   formResult2: FormGroup;
-   // formTime: FormGroup;
+  // formTime: FormGroup;
   private loading: boolean = false;
   private subs: Subscription = new Subscription;
   public viewCounter: number = 0;
   public stanowisko_id: number | null = null;
   public kategoria_id: number | null = null;
   public walka: any = null;
+  private settedArena: number | null = null;
 
   constructor(private formBuilder: FormBuilder, private refereeService: RefereeService, private route: ActivatedRoute,
-    private router: Router, private ui: UiService,private fightService: FightsService, public userService: UserService, private titleService: Title) {
+    private router: Router, private ui: UiService, private fightService: FightsService, public userService: UserService, private titleService: Title, private activeFightService: ActiveFightsService) {
     this.formUUID1 = this.formBuilder.group({
       constructor_uuid: [null, [Validators.required, Validators.minLength(36), Validators.maxLength(36)]]
     });
@@ -44,6 +47,9 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     this.formResult2 = this.formBuilder.group({
       fight_result: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(2)]]
     });
+    this.formArena = this.formBuilder.group({
+      arena: [null, [Validators.required]]
+    });
 
   }
 
@@ -52,10 +58,10 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     this.kategoria_id = Number(this.route.snapshot.paramMap.get('kategoria_id'));
     try {
       this.walka = JSON.parse((window as any).walka);
-    } catch(err) {
+    } catch (err) {
       this.backToPositions();
     }
-    if(!this.isCredentials || !this.walka) this.backToPositions();
+    if (!this.isCredentials || !this.walka) this.backToPositions();
     this.titleService.setTitle(`⚔️ ${this.walka.robot1_nazwa} vs ${this.walka.robot2_nazwa}`);
   }
 
@@ -63,13 +69,13 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     window.close()
   }
 
-  async goToSetFight() {
-    if(this.isFormUserUUIDValid) {
+  async goToArenaNumber() {
+    if (this.isFormUserUUIDValid) {
       this.loading = true;
       const uzytkownik_uuid1 = this.formUUID1.get('constructor_uuid')?.value;
       const uzytkownik_uuid2 = this.formUUID2.get('constructor_uuid')?.value;
-      const response1 = await this.refereeService.checkIfUserIsConstructorOfRobot(uzytkownik_uuid1, this.walka.robot1_uuid).catch(err => {return null});
-      const response2 = await this.refereeService.checkIfUserIsConstructorOfRobot(uzytkownik_uuid2, this.walka.robot2_uuid).catch(err => {return null});
+      const response1 = await this.refereeService.checkIfUserIsConstructorOfRobot(uzytkownik_uuid1, this.walka.robot1_uuid).catch(err => { return null });
+      const response2 = await this.refereeService.checkIfUserIsConstructorOfRobot(uzytkownik_uuid2, this.walka.robot2_uuid).catch(err => { return null });
       if (response1 !== undefined && response1 !== null && response1.body.pCzyJest === 1 && response2 !== undefined && response2 !== null && response2.body.pCzyJest === 1) {
         this.nextPage();
       } else {
@@ -79,15 +85,35 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     }
   }
 
+  async goToSetFight() {
+    if (this.isFormArenaValid) {
+      this.settedArena = this.formArena.get('arena')?.value;
+      this.loading = true;
+      if (this.settedArena) {
+        const response = await this.activeFightService.addCurrentFightOrTime(this.stanowisko_id!, this.kategoria_id!, this.settedArena, this.walka.robot1_id, this.walka.robot2_id);
+        if (response !== undefined && response !== null && response.message === "INFO: OK") {
+          this.nextPage();
+        } else {
+          this.loading = false;
+          this.ui.showFeedback("error", `Nie udało się dodać informacji o numerze areny`, 3);
+        }
+      } else {
+        this.nextPage();
+      }
+    }
+  }
+
   async setFight() {
-    if(this.isFormResultValid) {
+    if (this.isFormResultValid) {
       const wygrane_rundy1 = this.formResult1.get('fight_result')?.value;
       const wygrane_rundy2 = this.formResult2.get('fight_result')?.value;
       const decision = await this.ui.wantToContinue(`Czy potwierdzasz, że robot ${this.walka.robot1_nazwa} wygrał ${wygrane_rundy1} rund/y a robot ${this.walka.robot2_nazwa} wygrał ${wygrane_rundy2} rund/y?`)
       if (decision) {
         this.loading = true;
-        const response = await this.fightService.setFightResult(this.walka.walka_id, wygrane_rundy1, wygrane_rundy2).catch(err => {return null});
-        // const response2 = await this.refereeService.checkIfUserIsConstructorOfRobot(uzytkownik_uuid2, this.walka.robot2_uuid).catch(err => {return null});
+        const response = await this.fightService.setFightResult(this.walka.walka_id, wygrane_rundy1, wygrane_rundy2).catch(err => { return null });
+        if (this.settedArena) {
+          await this.activeFightService.removeCurrentFightOrTime(this.stanowisko_id!, this.kategoria_id!, this.settedArena);
+        }
         if (response !== undefined && response !== null && response.message === "INFO: OK") {
           this.backToPositions();
         } else {
@@ -98,7 +124,7 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     }
   }
 
-  async callTheConstructor(robot_uuid : string, robot_nazwa: string) {
+  async callTheConstructor(robot_uuid: string, robot_nazwa: string) {
     const decision = await this.ui.wantToContinue(`Jeśli potwierdzisz wykonanie procedury, do wszystkich konstruktorów robota ${robot_nazwa} zostanie wysłana wiadomość wzywająca ich do walki na ringu`)
     if (decision) {
       const response = await this.refereeService.callForConstructors(robot_uuid, this.stanowisko_id!, robot_nazwa);
@@ -106,23 +132,25 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
         this.ui.showFeedback("succes", `Konstruktorzy robota ${robot_nazwa} zostali wezwani`, 3);
       } else if (response !== undefined && response !== null && response.body.sendedCount == 0) {
         this.ui.showFeedback("loading", `Żaden z konstruktorów robota ${robot_nazwa} nie ma przypisanego numeru telefonu`, 5);
-      }else {
+      } else {
         this.ui.showFeedback("error", `Błąd!`, 3);
       }
     }
   }
 
   previousPage() {
-    if(this.viewCounter === 0) {
+    if (this.viewCounter === 0) {
       this.backToPositions();
     } else {
       this.viewCounter--;
     }
   }
 
-  async skip() {
-    const decision = await this.ui.wantToContinue(`Uwaga! Pominięcie etapu sprawdzania konstruktorów może skutkować przyznaniem punktów nieodpowieniemu robotowi`)
-    if(!decision) return;
+  async skip(info: boolean) {
+    if (info) {
+      const decision = await this.ui.wantToContinue(`Uwaga! Pominięcie etapu sprawdzania konstruktorów może skutkować przyznaniem punktów nieodpowieniemu robotowi`)
+      if (!decision) return;
+    }
     this.nextPage()
   }
 
@@ -137,12 +165,16 @@ export class AddFightResultComponent implements OnInit, OnDestroy {
     return this.formResult1.valid && this.formResult2.valid && !this.isLoading && this.isCredentials && wygrane_rundy1 != wygrane_rundy2;
   }
 
+  get isFormArenaValid() {
+    return this.formArena.valid && !this.isLoading && this.isCredentials;
+  }
+
   get isFormUserUUIDValid() {
     return this.formUUID1.valid && this.formUUID2.valid && !this.isLoading && this.isCredentials;
   }
 
   get isCredentials() {
-    return this.kategoria_id && this.stanowisko_id && this.walka && typeof(this.kategoria_id) === "number" && typeof(this.stanowisko_id) === 'number' && typeof(this.walka) === 'object';
+    return this.kategoria_id && this.stanowisko_id && this.walka && typeof (this.kategoria_id) === "number" && typeof (this.stanowisko_id) === 'number' && typeof (this.walka) === 'object';
   }
 
   get isLoading() {
